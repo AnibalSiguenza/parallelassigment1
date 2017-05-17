74539179d5351bd1081e81aef3f6a7f6c2e02591
Anibal <anibal.siguenza1@gmail.com>
2017-05-17 13:44:58 +0200

    check without .o or .d

--- student/histogram_par.d
+++
@@ -1,4 +0,0 @@
-student/histogram_par.o: student/histogram_par.c \
- /home/anibal/Documents/TUM/segundo_semestre/paralel_programming/tutorials/t1/assignment1/histogram.h
-
-/home/anibal/Documents/TUM/segundo_semestre/paralel_programming/tutorials/t1/assignment1/histogram.h:
