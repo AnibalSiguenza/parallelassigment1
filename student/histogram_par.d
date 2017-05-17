d864a768fc8f781c16587ddfc92cf2a13e09b4f7
Anibal <anibal.siguenza1@gmail.com>
2017-05-17 13:39:08 +0200

    First commit

---
+++ student/histogram_par.d
@@ -0,0 +1,4 @@
+student/histogram_par.o: student/histogram_par.c \
+ /home/anibal/Documents/TUM/segundo_semestre/paralel_programming/tutorials/t1/assignment1/histogram.h
+
+/home/anibal/Documents/TUM/segundo_semestre/paralel_programming/tutorials/t1/assignment1/histogram.h:
