c58b8fa04602ecc64475c76def63ae6ab8741a3d
Anibal <anibal.siguenza1@gmail.com>
2017-05-18 13:30:42 +0200

    Error for compilation optimization using num_threads-1

--- student/histogram_par.c
+++ student/histogram_par.c
@@ -5,14 +5,13 @@
 #include <ctype.h>
 
 #include "histogram.h"
-#include<pthread.h>
 
 struct pthread_args{
     int initIndex;
     int endIndex;
     block_t *blocks;
     char dummy[64];
-    histrogram_t histrogram;
+    histogram_t histogram;
 };
 
 void * countingInBlocks(void * ptr){
@@ -20,7 +19,31 @@ void * countingInBlocks(void * ptr){
     int initIndex=args->initIndex;
     int endIndex=args->endIndex;
     block_t *blocks=args->blocks;
-    histrogram_t histrogram=args->histogram;
+    int * histogram=args->histogram;
+	char current_word[20] = "";
+	int c = 0;
+    for(int i=0;i<NNAMES;i++){
+        histogram[i]=0;
+    }
+    for(int i=initIndex;i<endIndex;i++){
+        for (int j = 0; j < BLOCKSIZE; j++) {
+            // if the character is an alphabet store it to form a word.
+            if(isalpha(blocks[i][j])){
+                current_word[c++] = blocks[i][j];
+            }
+            // if the character is not an alphabet it is the end of a word.
+            // Compare the word with the list of names.
+            else {
+                current_word[c] = '\0';
+                for(int k = 0; k < NNAMES; k++)	{
+                    if(!strcmp(current_word, names[k]))
+                        histogram[k]++;
+                }
+                c = 0;
+            }
+        }
+    }
+    return NULL;
 }
 
 void get_histogram(int nBlocks, block_t *blocks, histogram_t histogram, int num_threads){
@@ -32,12 +55,12 @@ void get_histogram(int nBlocks, block_t *blocks, histogram_t histogram, int num_
     for(int i=0;i<num_threads-1;i++){
         arg[i].initIndex=i*nBlockPThreads;
         arg[i].endIndex=(i+1)*nBlockPThreads-1;
-        pthread_create(thread+i,NULL,&hello,arg+i);
+        pthread_create(thread+i,NULL,&countingInBlocks,arg+i);
     }
     arg[num_threads-1].initIndex=num_threads*nBlockPThreads;
     arg[num_threads-1].endIndex=num_threads*nBlockPThreads+resNBlocks;
-    pthread_create(thread+i,NULL,&countingInBlock,arg+num_threads-1);
-    for(int i=0;i<n_threads;i++){
+    pthread_create(thread+num_threads-1,NULL,&countingInBlocks,arg+num_threads-1);
+    for(int i=0;i<num_threads;i++){
         pthread_join(thread[i],NULL);  
         for(int j=0;j<NNAMES;j++){
             histogram[j]+=arg[i].histogram[j];
